db15ac98114238ddf8e98bcc3d73864ad60c77af
Alexey <chabrchikov@mail.ru>
2023-06-30 19:14:09 +0300

    task25

---
+++ Task25/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("введите число A");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число B");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int i = numberA;
+
+for (int j = 1; j < numberB; j++)
+{
+i = i * numberA;
+}
+Console.WriteLine("A в степени B равно: " + i);
